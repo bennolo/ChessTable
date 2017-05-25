199f62c0b99519510da84222f8e6cc4e80dea5ff
Benno Londner <blondner@kabelmail.de>
2017-05-25 20:34:35 +0200

    default Method FillBlackButtons added

--- src/tournament_v2/My_I.java
+++ src/tournament_v2/My_I.java
@@ -6,6 +6,8 @@
 package tournament_v2;
 
 import java.util.List;
+import javafx.scene.paint.Color;
+import static tournament_v2.Tournament_V2.Btn;
 
 
 public interface My_I {
@@ -17,6 +19,18 @@ public interface My_I {
                 .count();
         
         return start - (int)count;
+    }
+    
+    default void fillButtons() {
+        int k = 1;
+        int l = 1;
+        while (k <= 20 || l <= 20){
+            Btn[k][l].setStyle("-fx-background-color: black;");
+            Btn[k][l].setTextFill(Color.BLACK);
+            Btn[k][l].setDisable(true);
+            k++;
+            l++;
+        }
     }
     
 }
